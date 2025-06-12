d5ed4e13f194573c80a9c59d4c4a975b3eaff672
Tia Anderson <149900656+Miss-Tia@users.noreply.github.com>
2025-06-11 23:09:00 -0400

    Lesson 13

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_30_195746) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_04_004946) do
   create_table "coding_classes", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -110,6 +110,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_30_195746) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "current"
   end
 
   create_table "users", force: :cascade do |t|
